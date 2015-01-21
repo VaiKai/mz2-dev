aa4e3cd9358870d6513133f9204e007232ac7c56
Hetasser <hetasser+github@gmail.com>
2015-01-21 11:33:55 +0100

    modification de la page de login MH
    pour tester le changement d'innerhtml

--- data/js/testjp.js
+++ data/js/testjp.js
@@ -1,8 +1,28 @@
 self.port.on("loadExternalJS", function(url) {
+  function myTest2(testVar){
+    window.alert(testVar+'---');
+  }
+
   var mz_scriptURL=url+"/mz/tmp/testjp.js"
   console.log("URL du script appelé : " + mz_scriptURL);
-  self.port.emit("externalJSLoaded", $.getScript(mz_scriptURL));
+  $.getScript(mz_scriptURL, function(){
+		setTimeout(function(){
+			//test("tutu");
+      //console.log(myTestVar);
+		}, 1000);
+	})
+  
+function modifyFieldByTagName(tagName){
+        document.getElementsByTagName(tagName)[0].innerHTML += " chez Raistlin";
+}
+
+modifyFieldByTagName("legend");
+
+
+  self.port.emit("externalJSLoaded",'toto');
+  
   console.log("Fin du script appelé");
+  
 });
 
 
