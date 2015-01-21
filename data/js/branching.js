609b08c9361318ed30e7e68e9c909d897c3f0afa
Hetasser <hetasser+github@gmail.com>
2015-01-21 22:18:40 +0100

    Appel distant en XHR + nettoyage + renommage
    * Renommage de testjp.js en branching.js
    * Suppression de jquery (n'est plus utilisé)
    * Utilisation du XHR natif pour l'appel du script distant, permettant
    l'utilisation dans le code distant des fonctions définies dans l'addon.
    
    WARNING: utilisation de eval pour lancer les scripts distants
    Note : rajouter dans data/libs une api des fonctions communes de MZ

---
+++ data/js/branching.js
@@ -0,0 +1,42 @@
+self.port.on("loadExternalJS", function(url) {
+// fonction de test : modifie le cadre de connexion sur la page de login
+  function modifyFieldByTagName(tagName, param){
+          document.getElementsByTagName(tagName)[0].innerHTML += " " + param;
+  }
+
+  // récupération de l'url complete du script chargé
+  // cette url dépendra bien évidemment de l'url de la page en cours
+  // pour l'instant, c'est en dur
+  var mz_scriptURL=url+"/mz/tmp/externalScript.js"
+  console.log("URL du script appelé : " + mz_scriptURL);
+  
+  // chargement du script externe
+  function loadExternalScript(scriptURL){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", scriptURL);
+    xmlhttp.onreadystatechange = function()
+    {
+        if ((xmlhttp.status == 200) && (xmlhttp.readyState == 4))
+        {
+            eval(xmlhttp.responseText);
+            sendBackResult("externalJSLoaded",'done');
+        }
+    };
+    xmlhttp.send();
+  }
+  
+  loadExternalScript(mz_scriptURL);
+
+  
+  // fonction pour le retour au main.js
+  function sendBackResult(event, data){
+    self.port.emit(event, data); 
+    console.log("Fin du script appelé");
+  }  
+  
+});
+
+
+
+
+                      
